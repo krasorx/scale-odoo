7ceb9e2d6568a35f387281cbdb85157948d6a0c7
krasorx <luiesp27@gmail.com>
2023-11-07 22:31:17 +0000

    Initial setup scale app

---
+++ addons/scale/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'State',
+    'version': '1.0',
+    'category': 'Manufacturing',
+    'sequence': 27,
+    'summary': 'Truck weighting module',
+    'description': "",
+    'depends': [
+        'base_setup',
+        'mail',
+        'calendar',
+        'contacts',
+        'stock',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
